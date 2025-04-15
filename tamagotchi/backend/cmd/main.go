fdb2790274a737580a3b684ecdead2ac9502eef9
g.skrylnikov <grisha2182@gmail.com>
2025-04-15 03:15:19 +0300

    add server

--- tamagotchi/backend/cmd/main.go
+++ tamagotchi/backend/cmd/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"tamagochi-team/siverus/tamagochi/internal/config"
 	"tamagochi-team/siverus/tamagochi/internal/config/lib/logger/sl"
 	"tamagochi-team/siverus/tamagochi/internal/storage/postgres"
+
+	mwLogger "tamagochi-team/siverus/tamagochi/internal/http-server/middleware/logger"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
@@ -30,26 +36,39 @@ func main() {
 	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s",
 		cfg.Postgres.User, cfg.Postgres.Password, cfg.Postgres.DBName, cfg.Postgres.SSLMode)
 
-    storage, err := postgres.New(connStr)
+    _, err := postgres.New(connStr)
     if err != nil {
         log.Error("filed init storage", sl.Err(err))
         os.Exit(1)
     }
 
-    users, err := storage.GetAllUsers()
-    if err != nil {
-        fmt.Println("Ошибка:", err)
-        return
-    }
-
-    for _, u := range users {
-        fmt.Printf(u.UserName)
-    }
-
-
 	// TODO: init router -> chi
 
+	router := chi.NewRouter()
+
+	// middleware
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
 	// TODO: run server
+	log.Info("strating server", slog.String("address", cfg.Address))
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+
+	log.Error("server stopped")
 }
 
 func setupLogger(env string) *slog.Logger {
